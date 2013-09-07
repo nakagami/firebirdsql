bb08e657c2f846617a77ea1c9100fe520b2cad92
Hajime Nakagami <nakagami@gmail.com>
2013-09-07 12:12:15 +0900

    XSQLVAR

--- xsqlvar.go
+++ xsqlvar.go
@@ -23,100 +23,105 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 package firebirdsql
 
-class XSQLVAR:
-    type_length = {
-        SQL_TYPE_VARYING: -1,
-        SQL_TYPE_SHORT: 4,
-        SQL_TYPE_LONG: 4,
-        SQL_TYPE_FLOAT: 4,
-        SQL_TYPE_TIME: 4,
-        SQL_TYPE_DATE: 4,
-        SQL_TYPE_DOUBLE: 8,
-        SQL_TYPE_TIMESTAMP: 8,
-        SQL_TYPE_BLOB: 8,
-        SQL_TYPE_ARRAY: 8,
-        SQL_TYPE_QUAD: 8,
-        SQL_TYPE_INT64: 8,
-        SQL_TYPE_BOOLEAN: 1,
-        }
+import (
+    "bytes"
+    "encoding/binary"
+    "date"
+    "time"
+)
 
-    type_display_length = {
-        SQL_TYPE_VARYING: -1,
-        SQL_TYPE_SHORT: 6,
-        SQL_TYPE_LONG: 11,
-        SQL_TYPE_FLOAT: 17,
-        SQL_TYPE_TIME: 11,
-        SQL_TYPE_DATE: 10,
-        SQL_TYPE_DOUBLE: 17,
-        SQL_TYPE_TIMESTAMP: 22,
-        SQL_TYPE_BLOB: 0,
-        SQL_TYPE_ARRAY: -1,
-        SQL_TYPE_QUAD: 20,
-        SQL_TYPE_INT64: 20,
-        SQL_TYPE_BOOLEAN: 5,
-        }
 
-    def __init__(self, bytes_to_str):
-        self.bytes_to_str = bytes_to_str
-        self.sqltype = None
-        self.sqlscale = None
-        self.sqlsubtype = None
-        self.sqllen = None
-        self.null_ok = None
-        self.fieldname = ''
-        self.relname = ''
-        self.ownname = ''
-        self.aliasname = ''
+var xsqlvarTypeLength = map[int]int32 {
+    SQL_TYPE_VARYING: -1,
+    SQL_TYPE_SHORT: 4,
+    SQL_TYPE_LONG: 4,
+    SQL_TYPE_FLOAT: 4,
+    SQL_TYPE_TIME: 4,
+    SQL_TYPE_DATE: 4,
+    SQL_TYPE_DOUBLE: 8,
+    SQL_TYPE_TIMESTAMP: 8,
+    SQL_TYPE_BLOB: 8,
+    SQL_TYPE_ARRAY: 8,
+    SQL_TYPE_QUAD: 8,
+    SQL_TYPE_INT64: 8,
+    SQL_TYPE_BOOLEAN: 1,
+}
 
-    def io_length(self):
+var xsqlvarDisplayLength = map[int]int32 {
+    SQL_TYPE_VARYING: -1,
+    SQL_TYPE_SHORT: 6,
+    SQL_TYPE_LONG: 11,
+    SQL_TYPE_FLOAT: 17,
+    SQL_TYPE_TIME: 11,
+    SQL_TYPE_DATE: 10,
+    SQL_TYPE_DOUBLE: 17,
+    SQL_TYPE_TIMESTAMP: 22,
+    SQL_TYPE_BLOB: 0,
+    SQL_TYPE_ARRAY: -1,
+    SQL_TYPE_QUAD: 20,
+    SQL_TYPE_INT64: 20,
+    SQL_TYPE_BOOLEAN: 5,
+}
+
+
+type xSQLVAR struct {
+    var sqltype int32
+    var sqlscale int32
+    var sqlsubtype int32
+    var sqllen int32
+    var null_ok bool
+    var fieldname string
+    var relname string
+    var ownname string
+    var aliasname string
+}
+
+func NewXSQLVAR () *xSQLVAR {
+    x := new(xSQLVAR)
+    return x
+}
+
+
+func (x *xSQLVAR) ioLength() int32 {
         sqltype = self.sqltype
         if sqltype == SQL_TYPE_TEXT:
             return self.sqllen
         else:
-            return self.type_length[sqltype]
+            return xsqlvarTypeLength[sqltype]
 
-    def display_length(self):
-        sqltype = self.sqltype
-        if sqltype == SQL_TYPE_TEXT:
-            return self.sqllen
-        else:
-            return self.type_display_length[sqltype]
+}
 
-    def precision(self):
-        return None
+func (x *xSQLVAR) displayLenght() int 32 {
+    sqltype = self.sqltype
+    if sqltype == SQL_TYPE_TEXT:
+        return self.sqllen
+    else:
+        return self.type_display_length[sqltype]
+}
 
-    def __str__(self):
-        s  = '[' + str(self.sqltype) + ',' + str(self.sqlscale) + ',' \
-                + str(self.sqlsubtype) + ',' + str(self.sqllen)  + ',' \
-                + str(self.null_ok) + ',' + self.fieldname + ',' \
-                + self.relname + ',' + self.ownname + ',' \
-                + self.aliasname + ']'
-        return s
+func (x *xSQLVAR) _parseDate(raw_value []byte) date {
+    nday = bytes_to_bint(raw_value) + 678882
+    century = (4 * nday -1) // 146097
+    nday = 4 * nday - 1 - 146097 * century
+    day = nday // 4
 
-    def _parse_date(self, raw_value):
-        "Convert raw data to datetime.date"
-        nday = bytes_to_bint(raw_value) + 678882
-        century = (4 * nday -1) // 146097
-        nday = 4 * nday - 1 - 146097 * century
-        day = nday // 4
+    nday = (4 * day + 3) // 1461
+    day = 4 * day + 3 - 1461 * nday
+    day = (day + 4) // 4
 
-        nday = (4 * day + 3) // 1461
-        day = 4 * day + 3 - 1461 * nday
-        day = (day + 4) // 4
+    month = (5 * day -3) // 153
+    day = 5 * day - 3 - 153 * month
+    day = (day + 5) // 5
+    year = 100 * century + nday
+    if month < 10:
+        month += 3
+    else:
+        month -= 9
+        year += 1
+    return year, month, day
+}
 
-        month = (5 * day -3) // 153
-        day = 5 * day - 3 - 153 * month
-        day = (day + 5) // 5
-        year = 100 * century + nday
-        if month < 10:
-            month += 3
-        else:
-            month -= 9
-            year += 1
-        return year, month, day
-
-    def _parse_time(self, raw_value):
-        "Convert raw data to datetime.time"
+func (x *xSQLVAR) _parseTime(raw_value []byte) time {
         n = bytes_to_bint(raw_value)
         s = n // 10000
         m = s // 60
@@ -124,40 +129,42 @@ class XSQLVAR:
         m = m % 60
         s = s % 60
         return (h, m, s, (n % 10000) * 100)
+}
 
-    def value(self, raw_value):
-        if self.sqltype == SQL_TYPE_TEXT:
-            if self.sqlsubtype == 1:      # OCTETS
-                return raw_value
-            else:
-                return self.bytes_to_str(raw_value)
-        elif self.sqltype == SQL_TYPE_VARYING:
-            if self.sqlsubtype == 1:      # OCTETS
-                return raw_value
-            else:
-                return self.bytes_to_str(raw_value)
-        elif self.sqltype in (SQL_TYPE_SHORT, SQL_TYPE_LONG, SQL_TYPE_INT64):
-            n = bytes_to_bint(raw_value)
-            if self.sqlscale:
-                return decimal.Decimal(str(n) + 'e' + str(self.sqlscale))
-            else:
-                return n
-        elif self.sqltype == SQL_TYPE_DATE:
-            yyyy, mm, dd = self._parse_date(raw_value)
-            return datetime.date(yyyy, mm, dd)
-        elif self.sqltype == SQL_TYPE_TIME:
-            h, m, s, ms = self._parse_time(raw_value)
-            return datetime.time(h, m, s, ms)
-        elif self.sqltype == SQL_TYPE_TIMESTAMP:
-            yyyy, mm, dd = self._parse_date(raw_value[:4])
-            h, m, s, ms = self._parse_time(raw_value[4:])
-            return datetime.datetime(yyyy, mm, dd, h, m, s, ms)
-        elif self.sqltype == SQL_TYPE_FLOAT:
-            return struct.unpack('!f', raw_value)[0]
-        elif self.sqltype == SQL_TYPE_DOUBLE:
-            return struct.unpack('!d', raw_value)[0]
-        elif self.sqltype == SQL_TYPE_BOOLEAN:
-            return True if raw_value[0] else False
-        else:
+func (x *xSQLVAR) value(raw_value) interface{} {
+    if self.sqltype == SQL_TYPE_TEXT:
+        if self.sqlsubtype == 1:      # OCTETS
             return raw_value
+        else:
+            return self.bytes_to_str(raw_value)
+    elif self.sqltype == SQL_TYPE_VARYING:
+        if self.sqlsubtype == 1:      # OCTETS
+            return raw_value
+        else:
+            return self.bytes_to_str(raw_value)
+    elif self.sqltype in (SQL_TYPE_SHORT, SQL_TYPE_LONG, SQL_TYPE_INT64):
+        n = bytes_to_bint(raw_value)
+        if self.sqlscale:
+            return decimal.Decimal(str(n) + 'e' + str(self.sqlscale))
+        else:
+            return n
+    elif self.sqltype == SQL_TYPE_DATE:
+        yyyy, mm, dd = self._parse_date(raw_value)
+        return datetime.date(yyyy, mm, dd)
+    elif self.sqltype == SQL_TYPE_TIME:
+        h, m, s, ms = self._parse_time(raw_value)
+        return datetime.time(h, m, s, ms)
+    elif self.sqltype == SQL_TYPE_TIMESTAMP:
+        yyyy, mm, dd = self._parse_date(raw_value[:4])
+        h, m, s, ms = self._parse_time(raw_value[4:])
+        return datetime.datetime(yyyy, mm, dd, h, m, s, ms)
+    elif self.sqltype == SQL_TYPE_FLOAT:
+        return struct.unpack('!f', raw_value)[0]
+    elif self.sqltype == SQL_TYPE_DOUBLE:
+        return struct.unpack('!d', raw_value)[0]
+    elif self.sqltype == SQL_TYPE_BOOLEAN:
+        return True if raw_value[0] else False
+    else:
+        return raw_value
+}
 
