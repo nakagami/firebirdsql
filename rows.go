0a8aa84c1b955e782547145a0028d74a8950bf8a
Hajime Nakagami <nakagami@gmail.com>
2013-10-06 17:10:22 +0900

    add dummy Rows

---
+++ rows.go
@@ -0,0 +1,62 @@
+/*******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2013 Hajime Nakagami
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*******************************************************************************/
+
+package firebirdsql
+
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"io"
+)
+
+type firebirdsqlRows struct {
+	curNum int
+}
+
+func NewStubRows() *firebirdsqlRows {
+	return new(firebirdsqlRows)
+}
+
+func (rows *firebirdsqlRows) Columns() []string {
+	fmt.Println("firebirdsqlRows.Columns()")
+	return []string{"Column1", "Column2"}
+}
+
+func (rows *firebirdsqlRows) Close() (er error) {
+	fmt.Println("firebirdsqlRows.Close()")
+	return
+}
+
+func (rows *firebirdsqlRows) Next(dest []driver.Value) (err error) {
+	fmt.Println("firebirdsqlRows.Next()")
+	rows.curNum++
+	if rows.curNum < 5 {
+		dest[0] = rows.curNum
+		dest[1] = "ABC"
+	} else {
+		err = io.EOF
+	}
+
+	return
+}
