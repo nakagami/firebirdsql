30e280ece14e01473489127b55b42a8b4ef7cf99
Hajime Nakagami <nakagami@gmail.com>
2020-04-29 07:41:56 +0900

    update copyright year

--- attic/errmsgs.c
+++ attic/errmsgs.c
@@ -1,7 +1,7 @@
  /*******************************************************************************
  The MIT License (MIT)
 
- Copyright (c) 2009-2015 Hajime Nakagami
+ Copyright (c) 2009-2015, 2020 Hajime Nakagami
 
  Permission is hereby granted, free of charge, to any person obtaining a copy of
  this software and associated documentation files (the "Software"), to deal in
