85427e16ddd9d139db0fea4a0dd3f452b57aa1fe
the-goodies <gediminas.ziauberis@gmail.com>
2023-06-07 18:03:38 +0300

    xsqlvar.go: fix varchar (VARYING) type display length
    Length method of rows.ColumnTypes returns incorrect length (always -1) for VARCHAR (VARYING) type, while returning correct length for CHAR (TEXT) type. This patch fixes this.
    Applying change only to displayLength method seems to be sufficient, but I changed ioLength too (not sure if necessary).

--- xsqlvar.go
+++ xsqlvar.go
@@ -145,14 +145,14 @@ type xSQLVAR struct {
 }
 
 func (x *xSQLVAR) ioLength() int {
-	if x.sqltype == SQL_TYPE_TEXT {
+	if x.sqltype == SQL_TYPE_TEXT || x.sqltype == SQL_TYPE_VARYING {
 		return x.sqllen
 	}
 	return xsqlvarTypeLength[x.sqltype]
 }
 
 func (x *xSQLVAR) displayLength() int {
-	if x.sqltype == SQL_TYPE_TEXT {
+	if x.sqltype == SQL_TYPE_TEXT || x.sqltype == SQL_TYPE_VARYING {
 		return x.sqllen
 	}
 	return xsqlvarTypeDisplayLength[x.sqltype]
